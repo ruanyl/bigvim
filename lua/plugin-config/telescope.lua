4bc3f8791561d954b2f0739df3e7028b04e7e9de
Yulong Ruan <ruanyu1@gmail.com>
2022-01-19 01:01:47 +0200

    set telescope path_display to 'truncate'

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -1,11 +1,14 @@
 local actions = require('telescope.actions')
 require("telescope").setup({
-    defaults = {
-        mappings = {
-            i = {
-                ['<C-j>'] = actions.move_selection_next,
-                ['<C-k>'] = actions.move_selection_previous,
-            }
-        }
+  defaults = {
+    path_display = {
+      "truncate",
     },
+    mappings = {
+      i = {
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+      }
+    }
+  },
 })
